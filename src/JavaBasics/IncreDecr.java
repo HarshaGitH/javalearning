3a239be00f58082f3041522e22bdcc78a4924912
HarshaGitH <harsha988@hotmail.com>
2018-06-15 17:32:31 +0400

    Loopsnincrement

---
+++ src/JavaBasics/IncreDecr.java
@@ -0,0 +1,17 @@
+package JavaBasics;
+
+public class IncreDecr {
+
+	public static void main(String[] args) {
+	
+		int i =1;
+		System.out.println(i); // 1
+		
+		int j = i++;
+		
+		System.out.println(i); // 2
+		System.out.println(j);  // 1
+
+	}
+
+}
---
+++ src/JavaBasics/LoopsinJava.java
@@ -0,0 +1,44 @@
+package JavaBasics;
+
+public class LoopsinJava {
+
+	public static void main(String[] args) {
+	
+		//1.While loop 
+		// I want to print from 1-10 Disadvantage it will give infinite if we dont give incremental or decremental
+		
+		int i =1 ;//intialization
+		
+		while(i<=10)//conditional part
+			{
+			System.out.println(i);
+			i=i+1;//incremental or decremental part
+		}
+		
+		System.out.println("**************");
+		
+		
+		//2.For loop everything in the same line initi  condit incre/decre
+		for(int k =1;k<=10;k++) {
+			System.out.println(k);
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
